fe638bbf2fe15ec58cb6b8e4741f6df884072cc3
Prakash <Prakash@test.com>
2017-01-21 12:23:42 -0500

    Module-4 Assignment solution cleanup

--- module4-solution/src/menuapp/menudata.service.js
+++ module4-solution/src/menuapp/menudata.service.js
@@ -10,50 +10,6 @@ MenuDataService.$inject = ['$http', 'ApiBasePath']
 function MenuDataService($http, ApiBasePath) {
   var service = this;
 
-  // Dummy List of menu categories
-  // var categories = [];
-  //
-  // // Pre-populate
-  // categories.push({
-  //   short_name: "L",
-  //   name: "Lunch",
-  //   special_instruction: "Lunch Menu Items"
-  // });
-  // categories.push({
-  //   short_name : "D",
-  //   name: "Dinner",
-  //   special_instruction: "Dinner Menu Items"
-  // });
-
-  // List of category items
-  // var lunchMenuItems = [];
-  //
-  // // Pre-populate
-  // lunchMenuItems.push({
-  //   short_name: "L1",
-  //   name: "LunchItem1",
-  //   description: "Lunch Menu Items 1"
-  // });
-  // lunchMenuItems.push({
-  //   short_name : "L2",
-  //   name: "LunchItem2",
-  //   description: "Lunch Menu Items 2"
-  // });
-  //
-  // var dinnerMenuItems = [];
-  //
-  // // Pre-populate
-  // dinnerMenuItems.push({
-  //   short_name: "D1",
-  //   name: "DinnerItem1",
-  //   description: "Dinner Menu Items 1"
-  // });
-  // dinnerMenuItems.push({
-  //   short_name : "D2",
-  //   name: "DinnerItem2",
-  //   description: "Dinner Menu Items 2"
-  // });
-
   // Returns a promise using REST API endpoint: https://davids-restaurant.herokuapp.com/categories.json
   service.getAllCategories = function () {
     console.log("calling Http service for getting menu categories ");
@@ -77,19 +33,9 @@ function MenuDataService($http, ApiBasePath) {
            url: (ApiBasePath + "/menu_items.json?category=" + categoryShortName)
          }).then(function (result) {
           console.log(result.data.menu_items.length, "# of Menu Items found. Data:", result.data);
-          //return result.data.menu_items;
+          //return result.data.menu_items; //contains only menu_items list
           return result.data; //contains menu_items and Category information
       });
-
-
-    // if(categoryShortName === 'L'){
-    //   console.log("Returning - ", lunchMenuItems);
-    //   return lunchMenuItems;
-    // }
-    // else if (categoryShortName === 'D'){
-    //   console.log("Returning", dinnerMenuItems);
-    //   return dinnerMenuItems;
-    // }
   };
 
 }
